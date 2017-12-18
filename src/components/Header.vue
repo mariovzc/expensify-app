385f50540c869817d937b51a555dfd55ff11ee0e
Mario vizcaino <mario.vizcainos@gmail.com>
2017-12-18 10:11:09 -0500

    header component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <mt-header fixed :title="title"></mt-header>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style lang="scss">
+header {
+  height: 3.7em !important;
+  .mint-header-title{
+    font-weight: 500;
+    font-size: 1.6em
+  }
+}
+</style>
